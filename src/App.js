7d33442a9f9b78c99b751b9619c481a2aa05b7f3
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-09-19 15:04:03 -0300

    Feat: Rodapé implementado.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 function App() {
 
@@ -63,6 +64,7 @@ function App() {
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}
 
+      <Rodape />
     </div>
   );
 }
---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,29 @@
+import './Rodape.css';
+
+const Rodape = () => {
+    return (
+        <div className='rodape'>
+            <div className='social__icons'>
+                <a className='facebook__icon' href="facebook.com" target="_blank">
+                    <img src='./imagens/fb.png' alt='Icone do Facebook' />
+                </a>
+
+                <a className='twitter__icon' href="twitter.com" target="_blank">
+                    <img src='./imagens/tw.png' alt='Icone do Twitter' />
+                </a>
+                <a className='instagram__icon' href="instagram.com" target="_blank">
+                    <img src='./imagens/ig.png' alt='Icone do Instagram' />
+                </a>
+            </div>
+            <div className='rodape__logo'>
+                <img src='./imagens/logo.png' alt='Logo' />
+            </div>
+            <div className='rodape__dev'>
+                <span>Desenvolvido por: Rodrigo Duarte</span>
+            </div>
+
+        </div >
+    );
+}
+
+export default Rodape;
