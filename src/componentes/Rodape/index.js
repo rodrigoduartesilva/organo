281f1884a7722225c545c341604e3b6e7abf4813
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-09-22 17:54:49 -0300

    Feat: Implementação do icone de favorito.

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -27,7 +27,7 @@ const Rodape = () => {
         </section>
         <section>
             <p>
-                Desenvolvido por Alura.
+                Desenvolvido por Rodrigo Duarte.
             </p>
         </section>
     </footer>)
